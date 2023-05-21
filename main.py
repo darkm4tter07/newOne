5657c6d019ead5a4365964bbe2fb380a572fe313
darkm4tter07 <117939614+darkm4tter07@users.noreply.github.com>
2023-05-21 23:08:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     response = requests.get('https://api.publicapis.org/entries')
     if response.status_code == 200:
         print('Success !!')
+        data = response.json()
+        print(data)
     else:
         print('Error !!')
 
