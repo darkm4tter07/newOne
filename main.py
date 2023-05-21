8e5dd912124e6beff61700b40167a08ae2289048
darkm4tter07 <117939614+darkm4tter07@users.noreply.github.com>
2023-05-21 22:59:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    print("Hello, World!")
+import requests
 
-if __name__ == "__main__":
+def main():
+    response = requests.get('https://api.publicapis.org/entries')
+    if response.status_code == 200:
+        print('Success!')
+    else:
+        print('Error!')
+
+if __name__ == '__main__':
     main()
